e7fcad4a24966e084484f8b20edf1a501823b54e
Niharika Patel <niharika31.patel@hotmail.co.uk>
2021-04-19 15:34:07 +0100

    Facbook and advertisement

---
+++ Assets/Scripts/AddManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AddManagerScript : MonoBehaviour
+{
+    string googlePlayID = "4095891";
+    public string surfacingID = "bannerPlacement";
+    bool testMode = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.Initialize(googlePlayID, testMode);
+        StartCoroutine(ShowBannerWhenInitialized());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ShowBannerWhenInitialized()
+    {
+        while (!Advertisement.isInitialized)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        
+        Advertisement.Banner.Show(surfacingID);
+    }
+}
---
+++ Assets/Scripts/FacebookScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+
+public class FacebookScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        if(!FB.IsInitialized)
+        {
+            FB.Init();
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+
+    }
+
+    public void Share()
+    {
+        FB.ShareLink(contentTitle:"Play King Of the Board!", contentDescription:"Obsessed with this new game!", callback:OnShare);
+    }
+
+    private void OnShare(IShareResult result)
+    {
+        if(result.Cancelled || !string.IsNullOrEmpty(result.Error))
+        {
+            Debug.Log("sharing error: " + result.Error);
+        }
+        else if(!string.IsNullOrEmpty(result.PostId))
+        {
+            Debug.Log(result.PostId);
+        }
+        else
+        {
+            Debug.Log("Share sucessfull");
+        }
+    }
+
+}
